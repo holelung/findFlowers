876098ddfda9522fcce9cf997f3a6cabfbdde5cd
Jinwon <jinwon2812@naver.com>
2024-04-18 22:04:17 +0900

    인용문구 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,8 @@ progress_bar = st.progress(0)
 progress_text = st.empty()
 registered_images = 0 # 등록한 이미지를 저장하는 변수 
 
+
+
 uploaded_image = st.file_uploader("사진 찍은 이미지를 업로드하세요.", type=["jpg", "jpeg", "png"])
 def predict(image):
     # 이미지를 텐서로 변환
@@ -68,7 +70,22 @@ def predict(image):
         max_label = max(probabilities, key=probabilities.get)
         flower_name = max_label
         max_probability = probabilities[max_label]
-        result = "이 꽃은 {} 입니다!".format(max_label)
+        flower_info = {
+            "진달래": " 진달래는 보통 분홍색으로 많이 피고, 사랑과 기쁨을 뜻하는 꽃이랍니다!",
+            "초롱꽃": " 전세계에 오직 1종밖에 없는 희귀식물로 오직 우리나라에서만 피는 꽃이에요! 물이 많고 습도가 높은 곳에서 자라며 대부분 연한 자주색을 띈답니다! 성실을 뜻하는 꽃이에요!",
+            "능소화": " 여름에 피는 연한 주황색 꽃으로 꽃이 한 번만 피고 지지 않고 계속 피고, 또 피고 하여 꽃이 피는 기간 동안 계속 아름다운 꽃을 볼 수 있어요 추위에 약해서 9월이 되면 생기를 잃어버리는 꽃이에요 그리움과 기다림을 뜻하는 꽃이랍니다.",
+            "벚꽃": " 봄이 되면 벚나무에서 활짝 피는 꽃으로 분홍색, 하얀색 꽃잎이 아주 화려하고 아름다운 꽃이에요 일본을 상징하는 꽃으로 아름다운 정신, 정신적 사랑을 뜻하는 꽃이랍니다.",
+            "수레국화": " 국화의 한 종류로 30~90cm 남짓한 키에 보라색, 파란색 꽃잎이 특징적인 꽃이에요 4월~9월에 걸쳐서 피며 행복감을 뜻하는 꽃이랍니다!",
+            "개나리": " 봄에 피는 대한민국의 고유 식물로 전국에서 쉽게 볼 수 있어요 전통적으로 봄이 왔음을 알리는 꽃으로 우리에게 매우 친근한 꽃이랍니다 희망과 깊은 정을 뜻하는 꽃이에요",
+            "연꽃": " 인도에서 태어난 꽃으로 줄기는 우리가 먹는 연근으로 이용된답니다 물에서 피는 꽃이지만 논이나 늪지대에서도 찾아볼 수 있어요 소원해진 사랑, 깨끗한 마음을 뜻하는 꽃이랍니다",
+            "나팔꽃": " 아침 일찍 피었다가 낮에는 오므라들면서 시드는 신기한 꽃이에요 줄기가 덩굴지고 2m까지 감긴답니다 일편단심 사랑을 뜻하는 꽃이에요",
+            "무궁화": " 한국을 상징하는 사실상 국화로 생명력이 매우 강해서 많이 척박한 황경에서도 적응하고 피어나는 강한 꽃이에요 영원히 피고 또 피어서 지지 않는 다는 뜻을 가지고 있어요",
+            "장미": " 높이는 2~3m이며 5~6월에 빨강색, 보라색, 흰색 등 아름다운 색으로 피어나서 관상용으로 키우는 경우가 많아요! 장미는 빛을 매우 좋아하고, 공기가 맛있고 영양분이 많은 땅에서 피어난답니다! 보통 낭만적인 사랑을 뜻하는 꽃이랍니다",
+            "해바라기": " 콜럼버스가 아메리카를 발견한 이후 유럽에 소개되며 태양의 꽃으로 불리게 됐어요 줄기가 태양을 향해 굽어지는 특징이 있어서 해바라기라고 불리게 되었어요! 영원한 사랑을 뜻하는 꽃이에요"
+        }
+        if max_label in flower_info:
+            result = f"이 꽃은 **{max_label}** 입니다!"  # 꽃 이름을 강조체로 표시
+            result += flower_info[max_label]
     return flower_name, result
 
 
@@ -153,6 +170,7 @@ if "flowers" not in st.session_state:
         "image_url": "images/emty.png"
     },
 ]
+    
 
 # 꽃 이름만 추출
 flower_names = [flower["name"] for flower in st.session_state.flowers]
@@ -214,7 +232,7 @@ st.markdown("""
         max-width: 150px;  
         max-height: 200px;  
     }
- 
+
     .st-emotion-cache-1clstc5.eqpbllx1 {
         display:flex;
         justify-content : center;
